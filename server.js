aac2e9e0232624543add600bbc4eb3d6a951bc71
Frances Dalla Torre <francesdallatorre@MacBook-Pro.local>
2021-05-26 12:51:14 -0400

    added heroku link to whitelist

--- server.js
+++ server.js
@@ -4,16 +4,31 @@ const cors = require('cors')
 
 const studentsController = require('./controllers/students')
 const APP = express()
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 const DBNAME = 'students'
+const db = mongoose.connection;
 
+// mongoose.connect(`mongodb://localhost:27017/${DBNAME}`, { useNewUrlParser: true });
+//  mongoose.connection.once('open', () => {
+//      console.log('connected to mongoose...');
+//  });
 
-mongoose.connect(`mongodb://localhost:27017/${DBNAME}`, { useNewUrlParser: true });
-mongoose.connection.once('open', () => {
-    console.log('connected to mongoose...');
-});
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/${DBNAME}`
 
-const whitelist = ['http://localhost:3000']
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// mongoose.connection.once('open', () => {
+//     console.log('connected to mongoose...');
+// });
+
+db.on('open', () => { });
+
+const whitelist = ['http://localhost:3000', 'https://students-list-api.herokuapp.com']
 const corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
